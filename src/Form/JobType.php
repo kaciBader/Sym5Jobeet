4ecc0ae2eda4a27b1069cfd107563536a5b07616
kaci <bader.kaci@gmail.com>
2020-06-03 15:54:34 +0200

    add JobController
    - make crud for job entity
    - update index and show action and there correspong template
    - add the layout template for the application

---
+++ src/Form/JobType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Job;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('company')
+            ->add('logo')
+            ->add('url')
+            ->add('position')
+            ->add('location')
+            ->add('description')
+            ->add('how_to_apply')
+            ->add('token')
+            ->add('is_public')
+            ->add('is_activated')
+            ->add('email')
+            ->add('expires_at')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Job::class,
+        ]);
+    }
+}
